6b58b594ae6e990e65fec6f8bdffe07445b62420
Parker <parker.senter@gmail.com>
2023-07-21 02:18:55 +0000

    Added comments

--- src/createHero.py
+++ src/createHero.py
@@ -1,42 +1,64 @@
+#fetchone() fetchs all rows and looks over them
+#is similar to fetch all
+#get more in depth answer about it 
+
 import psycopg2
 from database.connection import execute_query, create_connection
 
-def create_hero():
+def createHero():
+
+    #Prompts user to put in input and stores the input
     name = input("What is your hero's name? ")
     aboutMe = input("Give a short description about your hero: ")
     biography = input("What's your hero's backstory? ")
-
     superpower = input("What superpower does your hero have? ")
 
+    #Creates the connection to the database
     connection = create_connection("postgres", "postgres", "postgres")
 
+    #checks if the connection to database was successful 
+    #if connection fails skips the try block and executes the except block
     if connection:
         try:
             with connection:
+
+                #creates a cursor object that lets SQL queries be made
                 with connection.cursor() as cursor:
+                    
+                    #checks if the user input superpower already exists within the abilities table
                     cursor.execute("SELECT id FROM ability_types WHERE name = %s", (superpower,))
                     existing_superpower_id = cursor.fetchone()
 
+                    #if the superpower already exists it gets the ID of that power and stores it
                     if existing_superpower_id:
                         superpower_id = existing_superpower_id[0]
+                    
+                    #if the power does not exist then it makes a new addition to the table and gives it an unique ID
                     else:
                         cursor.execute("INSERT INTO ability_types (name) VALUES (%s) RETURNING id", (superpower,))
                         superpower_id = cursor.fetchone()[0]
 
+                    #adds the hero and everything that goes with it to the respective table and gives the hero a unique ID
                     query = "INSERT INTO heroes (name, about_me, biography) VALUES (%s, %s, %s) RETURNING id"
                     params = (name, aboutMe, biography)
                     cursor.execute(query, params)
-
                     hero_id = cursor.fetchone()[0]
 
+                    #adds the new superpower to the respective table and the new ID made for it
                     ability_query = "INSERT INTO abilities (hero_id, ability_type_id) VALUES (%s, %s)"
                     ability_params = (hero_id, superpower_id)
                     cursor.execute(ability_query, ability_params)
 
+            #prints this to console if creating the new hero was successful
             print("You've successfully created your hero!")
+
+        #if an error occured this is displayed in console
         except psycopg2.Error as e:
             print("Error creating the hero:", e)
+        
+        #closes the connection to the database at the end no matter what happened
         finally:
             connection.close()
 
-create_hero()
+#calls the function to be ran
+createHero()
--- src/updateHero.py
+++ src/updateHero.py
@@ -7,69 +7,114 @@ def updateHero():
         try:
             with connection:
                 with connection.cursor() as cursor:
-
+                    
+                    #gets the names and ids from all the heros 
                     cursor.execute("SELECT id, name FROM heroes")
                     heroes = cursor.fetchall()
 
+                    #prints a list of all the heros into the console
                     print("List of existing heroes:")
                     for hero in heroes:
                         print(f"{hero[0]}. {hero[1]}")
 
+                    #while true loop, keeps loop running until its true
                     while True:
                         heroChoice = input("Enter the name of the hero you want to update: ")
                         try:
+
+                            #trys to turn the user input into a integer
                             heroChoice = int(heroChoice)
+
+                            #if the hero name/ID entered is not in the database the error is displayed
+                            #if it is in the database the code breaks and continues to the next part
                             if heroChoice not in [hero[0] for hero in heroes]:
                                 raise ValueError("Invalid hero. Please choose a name.")
                             break
+
+                        #if the code runs into a value error then it goes through this code block
+                        #when this happens it means the user did not put in an integer
                         except ValueError:
+
+                            #sets the hero ID to none
+                            #will be used to store the hero ID if a name is found
                             heroId = None
+
+                            #iterates through all the heros in the table
                             for hero in heroes:
+
+                                #checks if the entered name matches any in the database
+                                #if one is found then it breaks the loop
                                 if heroChoice == hero[1]:
                                     heroId = hero[0]
                                     break
+
+                            #find the corresponding ID to the name then breaks loop
                             if heroId:
                                 heroChoice = heroId
                                 break
+
+                            #if nothing is successful this error message is displayed
                             print("Invalid hero name. Please choose a valid ID or name.")
 
+                    #retrevies all the information on the current hero selected
+                    #stores everything in the heroInfo varaible 
                     cursor.execute("SELECT name, about_me, biography FROM heroes WHERE id = %s", (heroChoice,))
                     heroInfo = cursor.fetchone()
 
+                    #prints all of the hero information for the user to see
+                    #f makes it a string literal (like a template literal from JS)
+                    #\n is for a new line
+                    #{} <- vars inside of the braces are the hero and all the data from the table
                     print(f"\nCurrent Hero Information (ID: {heroChoice}):")
                     print(f"Name: {heroInfo[0]}")
                     print(f"About Me: {heroInfo[1]}")
                     print(f"Biography: {heroInfo[2]}")
 
+                    #asks the user what they would like to update in the information
                     updatedName = input("Enter the updated name (or leave empty to keep current): ")
                     updatedAboutMe = input("Enter the updated short description: ")
                     updatedBiography = input("Enter the updated backstory: ")
                     updatedSuperPower = input("Enter the updated superpower: ")
 
+                    #checks if any of the above input fields have text in them
                     if updatedName or updatedAboutMe or updatedBiography or updatedSuperPower:
+
+                        #if there was something typed into an input it will update the table with the new information
                         update_query = "UPDATE heroes SET name = COALESCE(%s, name), about_me = COALESCE(%s, about_me), biography = COALESCE(%s, biography) WHERE id = %s"
                         update_params = (updatedName, updatedAboutMe, updatedBiography, heroChoice)
                         cursor.execute(update_query, update_params)
 
+                        #after updating the above information it moves on to updating the powers
+                        #checks if the power already exists
                         cursor.execute("SELECT id FROM ability_types WHERE name = %s", (updatedSuperPower,))
                         existing_superpower_id = cursor.fetchone()
 
+                        #if the superpower exists them it sets its ID to the existing
                         if existing_superpower_id:
                             superpower_id = existing_superpower_id[0]
+
+                        #if it does not exist it creates a new power with a new ID
                         else:
                             cursor.execute("INSERT INTO ability_types (name) VALUES (%s) RETURNING id", (updatedSuperPower,))
                             superpower_id = cursor.fetchone()[0]
 
+                        #updates the abilities table with the new powers for that hero
                         update_ability_query = "UPDATE abilities SET ability_type_id = %s WHERE heroId = %s"
                         update_ability_params = (superpower_id, heroChoice)
                         cursor.execute(update_ability_query, update_ability_params)
 
+                        #prints that the changes were made to the console
                         print("Hero information updated successfully!")
+
+                    #if all input fields are blank it displays this message to the console showing nothing was changed about the hero
                     else:
                         print("No updates made. Hero information remains unchanged.")
 
+        #if an error occurs then this message is displayed
         except psycopg2.Error as e:
             print("Error updating the hero:", e)
+
+        #closes connection with database
         finally:
             connection.close()
 
